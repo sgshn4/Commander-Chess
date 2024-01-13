b25f40ace5e45fc3d1207568d3a2304f3cc44d65
sgshn4 <davidov.oleg2018@yandex.ru>
2024-01-13 14:42:35 +0300

    Added figures.

--- src/main/java/com/cs/vsu/pereslavtsev_oleg/comanser_chess/Game/CellFactory.java
+++
@@ -1,6 +0,0 @@
-package com.cs.vsu.pereslavtsev_oleg.comanser_chess.Game;
-public class CellFactory{
-    public static Cell createSell(int x, int y)  {
-        return new Cell(new Coordinates(x, y));
-    }
-}
--- src/main/java/com/cs/vsu/pereslavtsev_oleg/comanser_chess/Main.java
+++
@@ -1,7 +0,0 @@
-package com.cs.vsu.pereslavtsev_oleg.comanser_chess;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
